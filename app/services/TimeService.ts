2fbb783f626cde13b82149c2e38cdcc6bc283118
Daniel De Camp <theonly2017@gmail.com>
2023-04-02 12:25:11 -0500

    Change back to mysql

--- app/services/TimeService.ts
+++ app/services/TimeService.ts
@@ -1,8 +1,8 @@
 import BlogPost from "../types/BlogPost";
 
 export const getPostDateTime = (post: BlogPost) => {
-    const date = new Date(post.createdAt).toLocaleDateString();
-    const time = new Date(post.createdAt).toLocaleTimeString();
+  const date = new Date(post?.createdAt).toLocaleDateString();
+  const time = new Date(post?.createdAt).toLocaleTimeString();
 
-    return { date, time };
+  return { date, time };
 };
